4bd947be0d9771ae9b0580a7d1554fa2237efb2a
P. Michael Ossareh <ossareh@gmail.com>
2018-05-30 17:30:56 -0500

    modernize and lint test files
    as there are no real tests this doesn't lint cleanly yet

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,36 +1,28 @@
-'use strict';
-
-var _ = require('underscore'),
-    expect = require('expect.js'),
-    Plugin = require('../index.js'),
-    sinon = require('sinon'); // eslint-disable-line no-unused-vars
+import { noop } from 'underscore';
+import expect from 'expect.js';
+import Plugin from '../index';
 
 function stubServerless() {
-   return {
-      getProvider: function() {
-         return {};
-      },
-      cli: {
-         log: _.noop,
-         consoleLog: _.noop,
-         printDot: _.noop,
-      },
-   };
+	return {
+		getProvider: () => ({}),
+		cli: {
+			log: noop,
+			consoleLog: noop,
+			printDot: noop,
+		},
+	};
 }
 
-describe('serverless-plugin-cloudfront-lambda-edge', function() {
-   var plugin; // eslint-disable-line no-unused-vars
+describe('serverless-plugin-cloudfront-lambda-edge', () => {
+	let plugin;
 
-   beforeEach(function() {
-      plugin = new Plugin(stubServerless(), {});
-   });
+	beforeEach(() => {
+		plugin = new Plugin(stubServerless(), {});
+	});
 
-   describe('TODO', function() {
-
-      it('needs to be tested', function() {
-         expect(1).to.eql(1);
-      });
-
-   });
-
+	describe('TODO', () => {
+		it('needs to be tested', () => {
+			expect(1).to.eql(1);
+		});
+	});
 });
